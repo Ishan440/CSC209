7769dd42cd958c25e82baa050948bce78f3904d7
Ishan Sharma <ishmayoite440@gmail.com>
2019-06-03 12:58:17 -0400

    made 3n1, check if execurtable prints

---
+++ 3n1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+    int n;
+
+    if (argc != 2 || (n = atoi(argv[1])) <= 0) {
+        fprintf(stderr, "usage: %s n\nn must be a positive integer\n", argv[0]);
+        return(1);
+    while (n != 1)
+    {
+        if (n%2 == 0){
+            n = n / 2;
+        }
+        else {
+            n = 3*n + 1;
+        }
+        printf("%d\n", n);
+    }
+    
+    }
+}
