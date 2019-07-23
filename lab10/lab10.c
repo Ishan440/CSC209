9947f9ca2c724d0aa4bfb7e07e3b4f0f02019acb
Ishan Sharma <ishmayoite440@gmail.com>
2019-07-22 21:46:46 -0400

    lab10

---
+++ lab10/lab10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    FILE *fp;
+    char c;
+
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]); /*fprintf will redirect error to stderr with a 
+        better usage message */
+        return(1); }
+
+    if ((fp = fopen(argv[1], "r")) == NUL) {
+        perror(file);
+        exit(1);
+    while ((c = fgetc(fp)) != EOF)
+        putchar(c);
+    fclose(fp);
+
+    return(1);
+}
